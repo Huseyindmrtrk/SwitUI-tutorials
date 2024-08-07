204ffb7897934c146647786e3e57041ecf56b8c1
huseyindemirturk <hdemirturk@cyh.com.tr>
2024-08-07 17:14:13 +0300

    Initial Commit

---
+++ SwitUI-tutorials/SwitUI_tutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitUI_tutorialsApp.swift
+//  SwitUI-tutorials
+//
+//  Created by Hüseyin Demirtürk on 7.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwitUI_tutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
